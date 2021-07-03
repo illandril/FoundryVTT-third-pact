6513f1790a3874ce70d62802f0f149f588fe0698
Joe Spandrusyszyn <github@illandril.net>
2021-07-03 14:28:05 -0400

    MVP for arbitrary pact slot progression; #6

--- module/scripts/third-pact.js
+++ module/scripts/third-pact.js
@@ -1,4 +1,4 @@
-import { KEY as MODULE_NAME } from './module.js';
+import { KEY as MODULE_NAME, log } from './module.js';
 
 const SETTING_ROUNDING = 'roundingMode';
 const SETTING_ROUNDING__STANDARD = 'standard';
@@ -6,45 +6,96 @@ const SETTING_ROUNDING__DOWN = 'down';
 const SETTING_ROUNDING__UP = 'up';
 
 const THIRD_PACT_TYPE = 'illandril_thirdpact';
+const CUSTOM_PACT_TYPE_PREFIX = 'illandril_custompact_';
+const CUSTOM_PACT_TYPES = [
+  `${CUSTOM_PACT_TYPE_PREFIX}a`, `${CUSTOM_PACT_TYPE_PREFIX}b`, `${CUSTOM_PACT_TYPE_PREFIX}c`,
+];
+
+const getCustomPactTypeOptions = (customPactType) => {
+  let options;
+  const setting = game.settings.get(MODULE_NAME, customPactType);
+  if(setting) {
+    try {
+      options = JSON.parse(setting);
+    } catch (e) {
+      log.error(setting);
+      log.error(e);
+    }
+  } else {
+  }
+  if (!Array.isArray(options)) {
+    return null;
+  }
+  return options;
+};
 
 Hooks.once('init', () => {
-  CONFIG.DND5E.spellProgression[THIRD_PACT_TYPE] = 'illandril-third-pact.thirdpact';
+  CONFIG.DND5E.spellProgression[THIRD_PACT_TYPE] = `${MODULE_NAME}.thirdpact`;
 
   game.settings.register(MODULE_NAME, SETTING_ROUNDING, {
-    name: game.i18n.localize('illandril-third-pact.roundingMode'),
-    hint: game.i18n.localize('illandril-third-pact.roundingModeHint'),
+    name: game.i18n.localize(`${MODULE_NAME}.roundingMode`),
+    hint: game.i18n.localize(`${MODULE_NAME}.roundingModeHint`),
     scope: 'world',
     config: true,
     default: SETTING_ROUNDING__STANDARD,
     type: String,
     choices: {
-      standard: game.i18n.localize('illandril-third-pact.roundingMode-Standard'),
-      down: game.i18n.localize('illandril-third-pact.roundingMode-Down'),
-      up: game.i18n.localize('illandril-third-pact.roundingMode-Up'),
+      standard: game.i18n.localize(`${MODULE_NAME}.roundingMode-Standard`),
+      down: game.i18n.localize(`${MODULE_NAME}.roundingMode-Down`),
+      up: game.i18n.localize(`${MODULE_NAME}.roundingMode-Up`),
     },
     onChange: refreshPactSlots,
   });
 
+  for (let customPactType of CUSTOM_PACT_TYPES) {
+    game.settings.register(MODULE_NAME, customPactType, {
+      name: game.i18n.localize(`${MODULE_NAME}.${customPactType}.name`),
+      hint: game.i18n.localize(`${MODULE_NAME}.${customPactType}.hint`),
+      scope: 'world',
+      config: true,
+      default: '',
+      type: String,
+      onChange: refreshPactSlots,
+    });
+    CONFIG.DND5E.spellProgression[customPactType] = `${MODULE_NAME}.${customPactType}.name`;
+  }
+
   const basePrepareDerivedData = game.dnd5e.entities.Actor5e.prototype.prepareDerivedData;
-  game.dnd5e.entities.Actor5e.prototype.prepareDerivedData = function() {
+  game.dnd5e.entities.Actor5e.prototype.prepareDerivedData = function () {
     basePrepareDerivedData.call(this);
     derivePactSlots(this.data);
   };
 });
 
 const refreshPactSlots = () => {
+  log.debug('Refreshing Pact Slots');
   game.actors.forEach((actor) => {
-    const pactClass = actor.items.find((item) => {
-      if (item.type === 'class') {
-        const itemData = item.data.data;
-        if (itemData.spellcasting === 'pact' || itemData.spellcasting === THIRD_PACT_TYPE) {
-          return true;
-        }
-      }
+    const pactClass = someSpellcastingClass(actor, (itemData, progression) => {
+      return (
+        progression === 'pact' ||
+        progression === THIRD_PACT_TYPE ||
+        CUSTOM_PACT_TYPES.includes(progression)
+      );
     });
     if (pactClass) {
+      log.debug(`Actor[${actor.name}] has at least one pact class`);
       derivePactSlots(actor.data);
       actor.render(false);
+    }
+  });
+};
+
+const someSpellcastingClass = (actorData, fn) => {
+  actorData.items.some((item) => {
+    if (item.type === 'class') {
+      const itemData = item.data.data;
+      let progression = itemData.spellcasting;
+      if (typeof progression === 'object') {
+        progression = progression.progression;
+      }
+      if (progression) {
+        return fn(itemData, progression);
+      }
     }
   });
 };
@@ -52,15 +103,30 @@ const refreshPactSlots = () => {
 const derivePactSlots = (actorData) => {
   if (actorData.type !== 'character') {
     // Third-pact caster calculation is only supported for players
+    log.debug(`Actor[${actorData.name}] is not a character`);
+    return;
+  }
+
+  const spells = actorData.data.spells;
+
+  const hasCustomPactClass = someSpellcastingClass(actorData, (itemData, progression) => {
+    if (CUSTOM_PACT_TYPES.includes(progression)) {
+      log.debug(`Actor[${actorData.name}] has a custom pact slot class`);
+      calculateCustomPactSlots(spells, itemData, progression);
+      return true;
+    }
+  });
+  if (hasCustomPactClass) {
     return;
   }
 
   let { fullLevels, thirdLevels, isMultiClass } = countPactLevels(actorData);
   // Only fix pact slots if they have a 1/3 pact caster (Actor5e handles full-pact slots just fine)
   if (thirdLevels > 0) {
+    log.debug(`Actor[${actorData.name}] has at least one third-caster pact class`);
     const pactLevels = calculateEffectiveLevels(isMultiClass, fullLevels, thirdLevels);
     if (pactLevels > 0) {
-      calculatePactSlots(actorData.data.spells, pactLevels);
+      calculatePactSlots(spells, pactLevels);
     }
   }
 };
@@ -70,20 +136,13 @@ const countPactLevels = (actorData) => {
   let thirdLevels = 0;
   let classes = 0;
 
-  actorData.items.forEach((item) => {
-    if (item.type === 'class') {
-      const itemData = item.data.data;
-      let progression = itemData.spellcasting;
-      if(typeof progression === 'object') {
-        progression = progression.progression;
-      }
-      if (progression === 'pact') {
-        classes++;
-        fullLevels += itemData.levels;
-      } else if (progression === THIRD_PACT_TYPE) {
-        classes++;
-        thirdLevels += itemData.levels;
-      }
+  someSpellcastingClass(actorData, (itemData, progression) => {
+    if (progression === 'pact') {
+      classes++;
+      fullLevels += itemData.levels;
+    } else if (progression === THIRD_PACT_TYPE) {
+      classes++;
+      thirdLevels += itemData.levels;
     }
   });
   return { fullLevels, thirdLevels, isMultiClass: classes > 1 };
@@ -136,5 +195,40 @@ const calculatePactSlots = (spells, effectivePactLevel) => {
       spells.pact.max++;
     }
   }
+  spells.pact.value = Math.min(spells.pact.value, spells.pact.max);
+};
+
+const calculateCustomPactSlots = (spells, customPactClass, progression) => {
+  const customPactOptions = getCustomPactTypeOptions(progression);
+  let max = 0;
+  let level = 1;
+  if (customPactOptions) {
+    const classLevel = customPactClass.levels;
+    let levelOptions;
+    if (classLevel > customPactOptions.length) {
+      levelOptions = customPactOptions[customPactOptions.length - 1];
+    } else if (classLevel <= 0) {
+      levelOptions = null;
+    } else {
+      levelOptions = customPactOptions[classLevel - 1];
+    }
+    log.debug(`Custom pact options for level[${classLevel}]: ${JSON.stringify(levelOptions)}`)
+    if (levelOptions) {
+      if (levelOptions.slots) {
+        max = levelOptions.slots;
+      }
+      if (levelOptions.spellLevel) {
+        level = levelOptions.spellLevel;
+      }
+    }
+  }
+  const pactOverride = parseInt(spells.pact.override, 10);
+  if (!isNaN(pactOverride)) {
+    log.debug(`Actor has a pact slots override`);
+    max = Math.max(pactOverride, 1);
+  }
+  spells.pact = spells.pact || {};
+  spells.pact.level = level;
+  spells.pact.max = max;
   spells.pact.value = Math.min(spells.pact.value, spells.pact.max);
 };
